aa307eead5db3d5aec4da67e99ed30c3f7aac461
Shaojie Hong <hongsjcn@qq.com>
2015-12-21 14:04:41 +0800

    添加可左右滑出菜单tableviewCell

---
+++ SJViewController/SJBase/TableViewOthers/SJSwipTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SJSwipTableViewCell.h
+//
+//
+//  Created by Shaojie Hong on 15/12/20.
+//  Copyright © 2015年 Shaojie Hong. All rights reserved.
+//
+
+#import "SWTableViewCell.h"
+
+@interface SJSwipTableViewCell : SWTableViewCell<SWTableViewCellDelegate>
+
+@end
---
+++ SJViewController/SJBase/TableViewOthers/SJSwipTableViewCell.m
@@ -0,0 +1,81 @@
+//
+//  SJSwipTableViewCell.m
+//  
+//
+//  Created by Shaojie Hong on 15/12/20.
+//  Copyright © 2015年 Shaojie Hong. All rights reserved.
+//
+
+#import "SJSwipTableViewCell.h"
+
+@implementation SJSwipTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.leftUtilityButtons = [self leftButtons];
+        self.rightUtilityButtons = [self rightButtons];
+        self.delegate = self;
+    }
+    return self;
+}
+
+#pragma mark - SJSwipTableviewCell
+
+- (NSArray *)rightButtons {
+    NSMutableArray *rightUtilityButtons = [NSMutableArray new];
+    [rightUtilityButtons sw_addUtilityButtonWithColor:
+     [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0]
+                                                title:@"More"];
+    [rightUtilityButtons sw_addUtilityButtonWithColor:
+     [UIColor colorWithRed:1.0f green:0.231f blue:0.188 alpha:1.0f]
+                                                title:@"Delete"];
+    
+    return rightUtilityButtons;
+}
+
+- (NSArray *)leftButtons {
+    return nil;
+}
+
+#pragma mark - SWTableViewDelegate
+
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell scrollingToState:(SWCellState)state {
+    switch (state) {
+        case 0:
+            NSLog(@"utility buttons closed");
+            break;
+        case 1:
+            NSLog(@"left utility buttons open");
+            break;
+        case 2:
+            NSLog(@"right utility buttons open");
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell didTriggerLeftUtilityButtonWithIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+            NSLog(@"left button 0 was pressed");
+            break;
+        case 1:
+            NSLog(@"left button 1 was pressed");
+            break;
+        case 2:
+            NSLog(@"left button 2 was pressed");
+            break;
+        case 3:
+            NSLog(@"left btton 3 was pressed");
+        default:
+            break;
+    }
+}
+
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell didTriggerRightUtilityButtonWithIndex:(NSInteger)index {
+    [cell hideUtilityButtonsAnimated:YES];
+}
+
+@end
